6e84d62656928d1984fd2e2c89f35d36a750289e
Evgeny Temryazansky <evgenytemr@gmail.com>
2023-11-19 20:53:41 +0400

    add mini css extract plugin, add css modules settings

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import './Counter.scss'
+import s from './Counter.module.scss'
 
 export const Counter = () => {
     const [count, setCount] = useState(0);
@@ -9,7 +9,7 @@ export const Counter = () => {
     }
 
     return (
-        <div className={'counter'}>
+        <div className={s.counter}>
           <h1>{count}</h1>
             <button onClick={increment}>
                 increment
