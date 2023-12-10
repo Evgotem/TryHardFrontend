357faf0527e3a1ec3b7714238fa04cfe20245e23
Evgeny Temryazansky <evgenytemr@gmail.com>
2023-12-10 21:08:14 +0400

    add router dom, route config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,7 @@
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
-import {Suspense} from 'react';
+import {Link} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
-
+import {AppRouter} from "app/providers/router";
 
 
 export const App = () => {
@@ -17,12 +14,7 @@ export const App = () => {
       </button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
-      <Suspense fallback={<div>...Loading</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage/>}/>
-          <Route path={'/'} element={<MainPage/>}/>
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
